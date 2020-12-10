99657b0e4239caed74f4d9458ee9b8feccca1847
Jonas Ferreira <jonasfscc@gmail.com>
2020-12-10 16:10:05 -0300

    Move tests file; Add commit tests

--- repositories/tests.py
+++ repositories/tests.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.models import User
+from django.utils import timezone
 from rest_framework.test import APITestCase, APIClient
+from repositories.models import (
+    Repository,
+    Commit
+)
 
 
 class CommitTests(APITestCase):
     def setUp(self):
+        self.repo = Repository.objects.create(name='user/test_repo')
+        Commit.objects.create(
+            message='commit msg', sha='abcdef', author='user',
+            url='http://fakeurl.com/', date=timezone.now(),
+            avatar='http://fakeurl.com/avatar.jpg', repository=self.repo)
         self.url = '/api/commits/'
         self.user = User.objects.create_user(
             'user', 'user@user.com', 'user123')
@@ -14,9 +24,31 @@ class CommitTests(APITestCase):
         response = anon_client.get(self.url, format='json')
         self.assertEqual(response.status_code, 403)
 
-    def test_get_commits_as_user(self):
+    def test_get_commit_as_user(self):
         response = self.client.get(self.url, format='json')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        commit = response.data[0]
+        self.assertEqual(commit['sha'], 'abcdef')
+
+    def test_get_multiple_commits(self):
+        Commit.objects.create(
+            message='commit msg 2', sha='aaaaa', author='user',
+            url='http://fakeurl.com/', date=timezone.now(),
+            avatar='http://fakeurl.com/avatar.jpg', repository=self.repo)
+        Commit.objects.create(
+            message='commit msg 3', sha='bbbbb', author='user',
+            url='http://fakeurl.com/', date=timezone.now(),
+            avatar='http://fakeurl.com/avatar.jpg', repository=self.repo)
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 3)
+
+    def test_post_commit(self):
+        data = {}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(len(response.data), 1)
 
 
 class RepositoryTests(APITestCase):
