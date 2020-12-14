76546ed7ab9da4a9be8767f37d860b7386a600a1
Jonas Ferreira <jonasfscc@gmail.com>
2020-12-14 09:08:49 -0300

    Add breadcrumbs

--- assets/js/containers/CommitListContainer.js
+++ assets/js/containers/CommitListContainer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import * as commitAPI from '../api/CommitAPI';
 import CommitList from '../components/CommitList';
+import { Link } from 'react-router-dom';
 
 
 class CommitListContainer extends React.Component {
@@ -29,8 +30,34 @@ class CommitListContainer extends React.Component {
 
   render() {
     const {commits} = this.props;
+		let {author, repo} = this.props.match.params;
+		const isHome = (!author && !repo);
     return (
       <div>
+				<nav aria-label="breadcrumb" className="breadcrumb-nav">
+					<ol className="breadcrumb">
+						<li className={"breadcrumb-item"+ (isHome?" active":"")} aria-current="page">
+							{ isHome? 'Home' : (
+								<Link to="/" >
+									Home
+								</Link>
+							)}
+						</li>
+						{author? (
+							<li className="breadcrumb-item" aria-current="page"> Author </li>
+						) : ''}
+						{author? (
+							<li className="breadcrumb-item active" aria-current="page"> {author} </li>
+						) : ''}
+
+						{repo? (
+							<li className="breadcrumb-item" aria-current="page"> Repository </li>
+						) : ''}
+						{repo? (
+							<li className="breadcrumb-item active" aria-current="page"> {repo} </li>
+						) : ''}
+					</ol>
+				</nav>
         <CommitList commits={commits} />
       </div>
     );
